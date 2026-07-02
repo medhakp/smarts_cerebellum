aeee2723ecd7cf0093ec4d08582c685da347047b
medha <medhakporwal@gmail.com>
2026-07-02 18:53:49 -0400

    lme editor

--- smarts_cerebellum/util.py
+++ smarts_cerebellum/util.py
@@ -20,15 +20,6 @@ def subj_week_loop(df):
         yield subj_id, week
 
 
-def voxel_array(image_path):
-    """
-    Function to get image voxel array
-    Takes image path (str); returns voxel data as arr
-    """
-    image_read = nib.load(image_path)
-    return image_read.get_fdata()
-
-
 """
 week search functions: functions to search for files across different weeks (within-subject)
 """
@@ -195,50 +186,4 @@ def subj_path_search(reference_file, week):
     # our base_dir should be path-specific, so after you go to smarts_cerebellum, you should specify any subdirs before the subject (e.g. anatomicals, MNISym, etc.)
     # so this will look for a path like base_dir/subj/subj_WEEK_suffix
 
-def week_path_search(reference_file, subj_id):
-    """
-    Finds files from other weeks that match the structure of reference file.
-
-    Inputs:
-        reference_file (str): path to a file whose path will be used as reference.
-            That is, this file's path should have the structure that all other files from that week should have.
-        weeks: weeks in filepath
-    
-    Outputs:
-        week_paths (list[str]): paths to all files (including reference) that exist for weeks
-        week_available (list[str]): weeks whose files exist
-    """
-
-    ref_path = str(reference_file)
-
-    # find all places in path str with the week
-    match = re.search(r'W(\d+)', ref_path)
-    if not match:
-        print(f'could not find week token in reference path for {ref_path}')
-        return None
-    
-    ref_week_token = match.group(1) # return entire text that ws matched.
-
-    weeks_paths = []
-    weeks_available = []
-
-    weeks = find_weeks(subj_id)
-
-    for week in weeks:
-        # search for every week's file
-
-        # replace the week token(s) in reference image path with other tokens for new week path
-        #week_path = ref_path.replace(ref_week_token, f'W{week}')
-
-        week_path = re.sub(rf'W{ref_week_token}(?!\d)', f'W{week}', ref_path)
-
-        # this should be dead code
-        if not Path(week_path).exists():
-            print(f'skipping W{week}')
-            continue
-
-        weeks_paths.append(week_path)
-        weeks_available.append(week) # add weeks as integers
-
-    return weeks_paths, weeks_available
 #___________________________________________________________________________________
