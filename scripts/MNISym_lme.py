0378a248ce44dd1886c9661f3ce881b721c8ad51
medha <medhakporwal@gmail.com>
2026-07-07 21:22:29 -0400

    updates

--- scripts/MNISym_lme.py
+++ scripts/MNISym_lme.py
@@ -24,6 +24,7 @@ week_idx_dict = {
     'W52': 4
 }
 
+# only need to do the actual flip once; then we can comment those lines out? Or make the funciton have the option to run the flip or just search for the file path (or new fcn for switch path_name?)
 def flip_left_lesion(subj_path_dict, df, path, space, segment): # df = patients with left lesion
     for subj, week in subj_week_loop(df):
         flip = f'{path}/{subj}/{subj}_{week}_{space}_{segment}_coreg_reslice.nii.gz'
@@ -44,59 +45,79 @@ def flip_left_lesion(subj_path_dict, df, path, space, segment): # df = patients 
     return subj_path_dict
 
 
+def patients_run_lme(segment):
+
+    # patients need to flip their lesion (if LH lesion)
+    ref_subj = 'CU_2310'
+    subdir = f'MNISym_{segment}'
+    file_suffix = f'MNISym_{segment}_coreg_reslice.nii.gz'
+    subpath = os.path.join(gl.baseDir, subdir)
+    space = 'MNISym'
+    
+
+    # for saving lme output files
+    results_path = os.path.join(gl.baseDir, 'lme', f'{segment}')
+    prefix = f'patients_MNISymC_{segment}'
+
+    
+    # for each week: make dict with subjects and their file paths
+    subj_path_dict_patient = lme.make_week_dicts(df = patients_df, ref_subj = ref_subj, subdir = subdir, file_suffix = file_suffix)
+
+    # use flipped image for left lesion patients (so that lesion on right)
+    subj_path_dict_patient = flip_left_lesion(subj_path_dict = subj_path_dict_patient, df = left_lesion_df, path = subpath, space = space, segment = segment)
+
+
+    B, S, status_list = lme.main(subj_path_dict = subj_path_dict_patient, df = patients_df,
+                                                                            results_path = results_path, prefix = prefix)
+
+
+    # save convergence status to dataframe
+    patients_status_df = pd.DataFrame(status_list)
+    patients_status_df['patient_voxel']=1
+    patients_status_df['model'] = 'lme_intercept'
+    patients_status_df.to_csv(f'{results_path}/patients_voxel_status_{segment}.tsv', sep = '\t', index = False)
+
+    print(f'patients {segment} done!')
+
+
+
+def controls_run_lme(segment):
+    ref_control = 'UZP_1001'
+    subdir = f'MNISym_{segment}'
+    file_suffix = f'MNISym_{segment}_coreg_reslice.nii.gz'
+
+
+
+    # for saving lme output files
+    results_path = os.path.join(gl.baseDir, 'lme', f'{segment}')
+    prefix = f'patients_MNISymC_{segment}'
+
+
+    subj_path_dict_controls = lme.make_week_dicts(df = controls_df, ref_subj = ref_control, subdir = subdir, file_suffix = file_suffix)
+
+    B_c, S_c, status_list_c = lme.main(subj_path_dict = subj_path_dict_controls, df = controls_df,
+                                                                                        results_path = results_path, prefix = prefix)
+
+    # save convergence status to dataframe
+    controls_status_df = pd.DataFrame(status_list_c)
+    controls_status_df['patient_voxel']=0
+    controls_status_df['model'] = 'lme_intercept'
+    controls_status_df.to_csv(f'{results_path}/controls_voxel_status_{segment}.tsv', sep = '\t', index = False)
+
+    print(f'controls {segment} done!')
+
+
+
 
 #%%
+# PATIENTS
+#patients_run_lme('T1')
+patients_run_lme('GM')
+patients_run_lme('WM')
+patients_run_lme('CSF')
 
-# patients
-ref_subj = 'CU_2310'
-subdir = 'MNISym_T1'
-file_suffix = 'MNISym_T1_coreg_reslice.nii.gz'
-subpath = os.path.join(gl.baseDir, subdir)
-space = 'MNISym'
-segment = 'T1'
-
-# for saving lme output files
-results_path = os.path.join(gl.baseDir, 'lme')
-prefix = 'patients_MNISymC_T1'
-
-
-
-#%%
-# for each week: make dict with subjects and their file paths
-subj_path_dict_patient = lme.make_week_dicts(df = patients_df, ref_subj = ref_subj, subdir = subdir, file_suffix = file_suffix)
-
-# use flipped image for left lesion patients (so that lesion on right)
-subj_path_dict_patient = flip_left_lesion(subj_path_dict = subj_path_dict_patient, df = left_lesion_df, path = subpath, space = space, segment = segment)
-
-#%%
-B, S, status_list = lme.main(subj_path_dict = subj_path_dict_patient, df = patients_df,
-                                                                        results_path = results_path, prefix = prefix)
-
-
-# save convergence status to dataframe
-patients_status_df = pd.DataFrame(status_list)
-patients_status_df['patient_voxel']=1
-patients_status_df['model'] = 'lme_intercept'
-patients_status_df.to_csv(f'{results_path}/patients_voxel_status.tsv', sep = '\t', index = False)
-
-
-
-
-#%%
-# controls
-ref_control = 'UZP_1001'
-
-
-prefix_controls = 'controls_MNISymC_T1'
-
-
-subj_path_dict_controls = lme.make_week_dicts(df = controls_df, ref_subj = ref_control, subdir = subdir, file_suffix = file_suffix)
-
-B_c, S_c, status_list_c = lme.main(subj_path_dict = subj_path_dict_controls, df = controls_df,
-                                                                                    results_path = results_path, prefix = prefix_controls)
-
-# save convergence status to dataframe
-controls_status_df = pd.DataFrame(status_list_c)
-controls_status_df['patient_voxel']=0
-controls_status_df['model'] = 'lme_intercept'
-controls_status_df.to_csv(f'{results_path}/controls_voxel_status.tsv', sep = '\t', index = False)
+# CONTROLS
+#controls_run_lme('T1')
+controls_run_lme('GM')
+controls_run_lme('WM')
+controls_run_lme('CSF')
--- scripts/MNISym_lme_sum.py
+++ scripts/MNISym_lme_sum.py
@@ -4,11 +4,13 @@ import nibabel as nib
 import os
 
 import smarts_cerebellum.globals as gl
+
+
 # get the sum of images - e.g. from lme, get the total voxel intensity for that week
 
 lme_dir = os.path.join(gl.baseDir, 'lme')
 prefix = 'MNISymC_T1'
-suffix = 'lme_beta'
+
 #beta_name = 'MNISymC_T1_W0_lme_beta'
 
 template_img = '/home/UWO/mporwal2/Documents/GitHub/smarts_cerebellum/tpl-MNI152NLin2009cSymC_T1w.nii'
@@ -24,7 +26,7 @@ save_path = os.path.join(gl.baseDir, 'lme')
 
 # get the sum image's array
 def image_sum(group, weeks, shape, 
-              lme_dir = lme_dir, prefix = prefix, suffix = suffix): # weeks is a subset of the images whose sum is being taken
+              suffix, lme_dir = lme_dir, prefix = prefix): # weeks is a subset of the images whose sum is being taken
 
     week_image = np.zeros(shape)
 
@@ -46,27 +48,99 @@ week2 = ['W0', 'W4', 'W12'] # week 2 image = week 0 + week 1 + week 2
 week3 = ['W0', 'W4', 'W12', 'W24'] # ...
 week4 = ['W0', 'W4', 'W12', 'W24', 'W52']
 
+#%%
+# GET SUMMED IMAGES FOR EACH WEEK
 
 # PATIENTS
 group = 'patients'
 
+# betas
+beta_suffix = 'lme_beta'
+
 # get week 1 image
-week1_arr = image_sum(group = group, weeks = week1, shape = img_shape)
+week1_arr = image_sum(group = group, weeks = week1, shape = img_shape, suffix = beta_suffix)
 week1_img = nib.Nifti1Image(week1_arr, template_img.affine)
 nib.save(week1_img, f'{save_path}/{group}_{prefix}_W4_lme.nii.gz')
 
 # get week 2 image
-week2_arr = image_sum(group = group, weeks = week2, shape = img_shape)
+week2_arr = image_sum(group = group, weeks = week2, shape = img_shape, suffix = beta_suffix)
 week2_img = nib.Nifti1Image(week2_arr, template_img.affine)
 nib.save(week2_img,  f'{save_path}/{group}_{prefix}_W12_lme.nii.gz')
 
 # get week 3 image
-week3_arr = image_sum(group = group, weeks = week3, shape = img_shape)
+week3_arr = image_sum(group = group, weeks = week3, shape = img_shape, suffix = beta_suffix)
 week3_img = nib.Nifti1Image(week3_arr, template_img.affine)
 nib.save(week3_img,  f'{save_path}/{group}_{prefix}_W24_lme.nii.gz')
 
 # get week 4 image
-week4_arr = image_sum(group = group, weeks = week4, shape = img_shape)
+week4_arr = image_sum(group = group, weeks = week4, shape = img_shape, suffix = beta_suffix)
 week4_img = nib.Nifti1Image(week4_arr, template_img.affine)
 nib.save(week4_img,  f'{save_path}/{group}_{prefix}_W52_lme.nii.gz')
+
+# bse
+bse_suffix = 'lme_bse'
+
+# get week 1 image
+week1_arr = image_sum(group = group, weeks = week1, shape = img_shape, suffix = bse_suffix)
+week1_img = nib.Nifti1Image(week1_arr, template_img.affine)
+nib.save(week1_img, f'{save_path}/{group}_{prefix}_W4_lme_se.nii.gz')
+
+# get week 2 image
+week2_arr = image_sum(group = group, weeks = week2, shape = img_shape, suffix = bse_suffix)
+week2_img = nib.Nifti1Image(week2_arr, template_img.affine)
+nib.save(week2_img,  f'{save_path}/{group}_{prefix}_W12_lme_se.nii.gz')
+
+# get week 3 image
+week3_arr = image_sum(group = group, weeks = week3, shape = img_shape, suffix = bse_suffix)
+week3_img = nib.Nifti1Image(week3_arr, template_img.affine)
+nib.save(week3_img,  f'{save_path}/{group}_{prefix}_W24_lme_se.nii.gz')
+
+# get week 4 image
+week4_arr = image_sum(group = group, weeks = week4, shape = img_shape, suffix = bse_suffix)
+week4_img = nib.Nifti1Image(week4_arr, template_img.affine)
+nib.save(week4_img,  f'{save_path}/{group}_{prefix}_W52_lme_se.nii.gz')
 # %%
+
+# make summarized dataframe using anatomical atlas Diedrichsen_2009
+import SUITPy as suit
+import os
+import smarts_cerebellum.globals as gl
+
+save_path = os.path.join(gl.baseDir, 'lme')
+all_weeks = ['W0', 'W4', 'W12', 'W24', 'W52']
+
+
+# finds one file per week
+def _image_paths(group, suffix, prefix, weeks = all_weeks):
+    images = []
+    for week in weeks:
+        file = f'{save_path}/{group}_{prefix}_{week}_{suffix}.nii.gz'
+        images.append(file)
+    return images
+
+
+
+# dataframe will just be: image, roi, (other atlas_summary cols), isPatient (add this one), week (add)
+
+def summary_df(images, param, isPatient, the_atlas = 'Diedrichsen_2009', maps = 'atl_Anatom', space = 'MNISym'):
+     suit.fetch_atlas(the_atlas)
+
+     df = suit.summarize_data(images = images, atlas = the_atlas, maps = maps, space = space,
+                              stats = ['mean', 'median', 'nansum']
+                              )
+     df['isPatient'] = isPatient # bool
+     df['param'] = param # betas or bse?
+     # save week somewhere...maybe we can search for the week in image_name? Or code it later from image (number in atlas)
+
+     return df
+#%%
+# PATIENTS - summed betas and summed bse
+group = 'patients'
+prefix = 'MNISymC_T1'
+suffix = 'lme'
+
+
+patient_images = _image_paths(group = group, suffix = suffix, prefix = prefix)
+patients_df = summary_df(images = patient_images, isPatient = 1)
+
+# save betas_df, bse_df together
