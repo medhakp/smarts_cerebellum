b8fb4f082e1703d533ef7c4a084116995a8eacea
medha <medhakporwal@gmail.com>
2026-07-23 13:12:58 -0400

    update flip_lesion for weeks

--- scripts/flip_lesion.py
+++ scripts/flip_lesion.py
@@ -4,15 +4,9 @@ from pathlib import Path
 import os
 import smarts_cerebellum.globals as gl
 
-# flop slope images from regression
-
 def FlipLR(image):
     """
     Simple flip: flips image along x-axis (L-R flip)
-
-    Input: image (Nifti or string)
-
-    Output: Nifti image
     """
     if type(image) == str:
         image = nib.load(image)
@@ -25,30 +19,66 @@ def FlipLR(image):
     
     return flipped_img
 
+
 def flip_left_lesion(path, 
                      left_lesion_df, # e.g. from p_df, subjs with LesionSide == 'left '
                      space='MNISymC', 
                      segment='T1', 
-                     metric='slope'):
+                     metric='_slope',
+                     use_weeks = False):
     '''
     flip left lesion to the right
     '''
-
+    flips = []
     for subj in left_lesion_df.subj_id.unique():
-        flip = f'{path}/{subj}/{subj}_{space}_{segment}_{metric}.nii.gz'
+        if use_weeks == False:
+            flip = f'{path}/{subj}/{subj}_{space}_{segment}{metric}.nii.gz'
 
-        if not Path(flip).is_file():
-            continue
+            if not Path(flip).is_file():
+                continue
+            flips.append(flip)
 
-        flipped = FlipLR(flip)
+            flipped = FlipLR(flip)
 
-        nib.save(flipped, f'{path}/{subj}/{subj}_{space}_{segment}_{metric}_FlipLR.nii.gz')
+            nib.save(flipped, f'{path}/{subj}/{subj}_{space}_{segment}{metric}_FlipLR.nii.gz')
+        
+        else: # use_weeks == True
+            # get all their weeks
+            subj_df = left_lesion_df[left_lesion_df.subj_id == subj]
+            weeks = subj_df.Week.unique()
+
+            for _week in weeks:
+                week = _week.strip()
+                flip = f'{path}/{subj}/{subj}_{week}_{space}_{segment}{metric}.nii.gz'
+                
+                if not Path(flip).is_file():
+                    continue
+                                
+                flipped = FlipLR(flip)
+                
+                nib.save(flipped, f'{path}/{subj}/{subj}_{week}_{space}_{segment}{metric}_FlipLR.nii.gz')
+
+
 
 if __name__=='__main__':
     p_df = pd.read_csv(os.path.join(gl.baseDir, 'participants.tsv'), sep = '\t')
     left_lesion_df = p_df[p_df.LesionSide == 'left ']
+
+    """
+    # flip regression slopes
     path = os.path.join(gl.baseDir, 'regression')
 
     segments = ['T1', 'WM_mod', 'GM_mod', 'CSF_mod'] # ran regression on modulated tissue volumes
     for segment in segments:
-        flip_left_lesion(path, left_lesion_df, segment = segment, metric = 'slope')
+        flip_left_lesion(path, left_lesion_df, segment = segment, metric = '_slope')
+    """
+    # flip normalized segment images
+    space = 'MNISymC'
+    segments = ['WM', 'GM', 'CSF']
+    for segment in segments:
+        seg_path = os.path.join(gl.baseDir, f'{space}_{segment}')
+        flip_left_lesion(seg_path, left_lesion_df, segment = segment, metric = '_mod', use_weeks = True)
+    
+    # flip normalized T1 images
+    t1_path = os.path.join(gl.baseDir, f'{space}_T1')
+    flip_left_lesion(t1_path, left_lesion_df, segment = 'T1', metric = '', use_weeks = True)
