484f741061a6a58dfe2c62ee1d6af27cca37e93c
Marco Emanuele <97095997+mnlmrc@users.noreply.github.com>
2026-08-06 10:59:51 -0400

    updates

--- smarts_cerebellum/globals.py
+++ smarts_cerebellum/globals.py
@@ -7,3 +7,8 @@ Hem = ['l', 'r']
 side = ['Contralesional', 'Ipsilesional', ]
 
 weeks = [0, 4, 12, 24, 52]
+
+rois = {'Nettekoven2024': ['M1', 'M2', 'M3', 'M4',
+                           'A1', 'A2', 'A3', 
+                           'D1', 'D2', 'D3', 'D4', 
+                           'S1', 'S2', 'S3', 'S4', 'S5']}
